f6c72cf3125fb09fa86ed053534c9124b4fdb36e
Silica163 <96188282+Silica163@users.noreply.github.com>
2021-12-21 14:09:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {GameManager} = require("./sub/game-manager.js");
 const {MapManager} = require("./sub/map-manager.js");
 const {OutputManager} = require("./sub/output-manager.js");
 //const {InputManager} = require("./sub/input-manager.js")
-const {map} = require("./sub/require-in-all.js")
+//const {map} = require("./sub/require-in-all.js")
 
 //https://www.kindacode.com/article/node-js-colorizing-console-log-output/
 //https://www.npmjs.com/package/node-mouse
